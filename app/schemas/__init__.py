a329851449f1ea7729f453bf027cf12fbe4d7d21
Gonzalo <g.sanzlalinde@outlook.com>
2025-12-23 01:13:18 +0000

    Update schemas init imports

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -1,4 +1,5 @@
-from .user import UserCreate, UserResponse
+from .user import UserCreate, UserResponse, UserUpdate, PasswordChange
 from .token import Token, TokenData
 from .manga import MangaCreate, MangaResponse, MangaISBN
 from .collection import CollectionAddManga, CollectionUpdateManga, CollectionResponse
+from .stats import AuthorStatsResponse, PublisherStatsResponse, CollectionStatsResponse
